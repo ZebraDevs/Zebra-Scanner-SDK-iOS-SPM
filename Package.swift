05c37456eb2718e0cd5cbe8a564de2af1973828d
Prabhath Wijerathne <PW2718@zebra.com>
2024-06-18 18:15:28 +0530

    Updated the Package manifest file

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ZebraScannerSDK",
+    platforms: [
+            .iOS(.v15)
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -14,7 +17,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "ZebraScannerSDK")
+        .binaryTarget(
+                   name: "ZebraScannerSDK",
+                   url: "https://github.com/zebratechnologies/Zebra-Scanner-SDK-iOS-SPM/releases/download/1.4.42-beta/ZebraScannerSDK.xcframework.zip",
+                   checksum: "a62bf35012e2991f355dabfc108bb10c186b1d0af9d29cb09deff410a144726d"
+               )
     ]
 )
